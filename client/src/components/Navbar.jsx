dadf71dda1a7a9284e1ed2e9e7c02f4149b121ad
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-05 01:57:50 +0530

    Complete navbar component for sm devices

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -52,6 +52,71 @@ const Navbar = () => {
         </Link>
       </div>
       {/* Small screen navigation */}
+      <div className="sm:hidden flex justify-between items-center relative">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+          <img
+            src={thirdweb}
+            alt="user"
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+
+        <img
+          src={menu}
+          alt="menu"
+          className="w-[34px] h-[34px] object-contain cursor-pointer"
+          onClick={() => setToggleDrawer(!toggleDrawer)}
+        />
+
+        <div
+          className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${
+            !toggleDrawer ? 'translate-y-[100vh]' : 'translate-y-0'
+          } transition-all duration-700`}
+        >
+          <ul className="mb-4">
+            {navlinks.map((link) => (
+              <li
+                key={link.name}
+                className={`flex p-4 ${
+                  isActive === link.name && 'bg-[#3a3a43]'
+                }`}
+                onClick={() => {
+                  setIsActive(link.name);
+                  setToggleDrawer(false);
+                  navigate(link.link);
+                }}
+              >
+                <img
+                  src={link.imgUrl}
+                  alt={link.name}
+                  className={`w-[24px] h[24px] object-contain ${
+                    isActive === link.name ? 'greyscale-0' : 'grayscale'
+                  }`}
+                />
+                <p
+                  className={`ml-[20px] font-epilogue font-semibold text-[14px] ${
+                    isActive === link.name ? 'text-[#1dc071]' : 'text-[#808191]'
+                  }`}
+                >
+                  {link.name}
+                </p>
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex m4">
+            <CustomButton
+              btnType="button"
+              title={address ? 'Create a campaign' : 'Connect'}
+              styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+              handleClick={() => {
+                if (address) navigate('create-campaign');
+                else 'connect()';
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
