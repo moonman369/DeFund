ee89b4efdf6132c08650d1a08db9ab13b0f7baf8
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-05 01:18:13 +0530

    Create search bar in navbar component and export custombutton component from index.js

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import CustomButton from './CustomButton';
+import { logo, menu, search, thirdweb } from '../assets';
+import { navlinks } from '../constants';
 
 const Navbar = () => {
-  return <div>Navbar TEST</div>;
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState('dashboard');
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  const address = '0xa78d7e251df2b2c16738053577283c';
+
+  return (
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
+        <input
+          type="text"
+          placeholder="Search for campaigns"
+          className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"
+          name=""
+          id=""
+        />
+
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
+          <img
+            src={search}
+            alt="search"
+            className="w-[15px] h-[15px] object-contain"
+          />
+        </div>
+      </div>
+
+      <div className="sm:flex hidden flex-row justify-end gap-4">
+        <CustomButton
+          btnType="button"
+          title={address ? 'Create a campaign' : 'Connect'}
+          styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+          handleClick={() => {
+            if (address) navigate('create-campaign');
+            else 'connect()';
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
