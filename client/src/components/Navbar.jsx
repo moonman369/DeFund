3c7e3c5f02a5e97bd6b79757dd8d901ac3009aab
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-05 01:30:14 +0530

    Complete create campaign button component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -40,7 +40,18 @@ const Navbar = () => {
             else 'connect()';
           }}
         />
+
+        <Link to="/profile">
+          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+            <img
+              src={thirdweb}
+              alt="user"
+              className="w-[60%] h-[60%] object-contain"
+            />
+          </div>
+        </Link>
       </div>
+      {/* Small screen navigation */}
     </div>
   );
 };
