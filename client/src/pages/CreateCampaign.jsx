b4c3806f722ce8cb73095f876d40276ecc404730
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-07 00:15:12 +0530

    Complete all form fields and submit btn

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -23,7 +23,7 @@ const CreateCampaign = () => {
     <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
       {isLoading && 'Loader...'}
       <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
-        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">
           Start a campaign
         </h1>
       </div>
@@ -79,6 +79,14 @@ const CreateCampaign = () => {
             value={form.deadline}
             handleChange={() => {}}
           />
+
+          <div className="flex justify-center items-center mt-[40px]">
+            <CustomButton
+              btnType="submit"
+              title="Submit new campaign"
+              styles="bg-[#1dc071]"
+            />
+          </div>
         </div>
       </form>
     </div>
