f208b0f60ee957e3a9d03826e0dd7bf6b4817c40
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-07 00:58:17 +0530

    Wrap App component inside ContextProvider. Rename context/index.js -> *.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM  from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 import App from './App';
-import './index.css'
+import './index.css';
+import { StateContextProvider } from './context';
 
 root.render(
-    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
-        <Router>
-            <App />
-        </Router>
-    </ThirdwebProvider>
-)
-
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider>
+);
