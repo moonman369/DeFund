b5a9b5f1bf8d26374d50e8ccdc388aa542bcb813
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-07 00:51:27 +0530

    Create a state context to use the contract call wrappers from across the components

---
+++ client/src/context/index.js
@@ -0,0 +1,55 @@
+import React, { useContext, createContext } from 'react';
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    '0x120EA1395144737693710c9Ed8eF390ba5C64b19'
+  );
+
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    'createCampaign'
+  );
+
+  const address = useAddress();
+
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address, // caller
+        form.title,
+        form.description,
+        form.target,
+        new Date(form.deadline).getTime(),
+        form.image,
+      ]);
+      console.log('Contract call success', data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        createCampaign: publishCampaign,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
