278bf70179e91e3f58cc20ca5a8f1359ee7e0cc2
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-06 23:46:23 +0530

    Basic layout of create campaign -Add basic structure of create campaign page - Add formfield component for accepting campaign details - Export formfield component from components/index.js

---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FormField = () => {
+  return <div>FormField</div>;
+};
+
+export default FormField;
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ethers } from 'ethers';
+import { money } from '../assets';
+import { CustomButton, FormField } from '../components';
+import { checkIfImage } from '../utils';
 
 const CreateCampaign = () => {
-  return (
-    <div>CreateCampaign</div>
-  )
-}
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: '',
+    title: '',
+    description: '',
+    target: '',
+    deadline: '',
+    image: '',
+  });
 
-export default CreateCampaign
+  const handleSubmit = () => {};
+
+  return (
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+      {isLoading && 'Loader...'}
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px]">
+          Start a campaign
+        </h1>
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="w-full mt-[65px] flex flex-col gap-[30px]"
+      >
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField
+            labelName="Your Name *"
+            placeholder="John Doe"
+            inputType="text"
+            value={form.name}
+            handleChange={() => {}}
+          />
+          <FormField
+            labelName="Campaign Title *"
+            placeholder="Enter a title"
+            inputType="text"
+            value={form.title}
+            handleChange={() => {}}
+          />
+          <FormField
+            labelName="Your Name *"
+            placeholder="John Doe"
+            inputType="text"
+            value={form.name}
+            handleChange={() => {}}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateCampaign;
