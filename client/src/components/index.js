d5db9940212a9fc0bf430b65112f93805ba40251
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-09 02:24:07 +0530

    Complete fund card component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Navbar } from './Navbar';
 export { default as CustomButton } from './CustomButton';
 export { default as FormField } from './FormField';
 export { default as DisplayCampaigns } from './DisplayCampaigns';
+export { default as FundCard } from './FundCard';
