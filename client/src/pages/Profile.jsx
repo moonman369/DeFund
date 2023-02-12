f56be5d2ca89ce493f2a59d533176e129bd8da87
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-12 11:58:28 +0530

    Create profile page - Add getUserCampaigns fn to filter campgns created by current user only

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { DisplayCampaigns } from '../components';
+import { useStateContext } from '../context';
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
 
-export default Profile
+  const { address, contract, getUserCampaigns } = useStateContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <div>
+      <DisplayCampaigns
+        title="All campaigns"
+        isLoading={isLoading}
+        campaigns={campaigns}
+      />
+    </div>
+  );
+};
+
+export default Profile;
