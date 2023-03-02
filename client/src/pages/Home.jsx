34a9a5e0524bab459916668fd75ceeeda37a0414
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-03-03 00:01:51 +0530

    Add homepage text

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,8 +37,30 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <p className="text-[80px] max-[400px]:text-[35px] font-bold font-epilogue text-center bg-clip-text bg-gradient-to-r from-[#0658c2] to-[#12be6b] text-transparent">
+      <h1 className="text-[80px] max-[400px]:text-[35px] font-bold font-epilogue text-center bg-clip-text bg-gradient-to-r from-[#0658c2] to-[#12be6b] text-transparent">
         Welcome to DeFund
+      </h1>
+
+      <p className="text-justify text-[#c8f7ec] font-epilogue text-[18px] mx-[3rem] my-4">
+        This is where crowdfunding meets the world of Decentralized Web. Using
+        DeFund, you can create customized campaigns representing your ideas and
+        raise funds in crypto. DeFund is a Crowdfunding platform based on the
+        famous Ethereum Network. The fact that DeFund uses blockchain technology
+        under the hood implies that it comes with the perks of decentralization.{' '}
+        <br />
+        <br /> It's Salient Features are as follows:
+        <li>Completely Decentralized</li>
+        <li>Highly Secured</li>
+        <li>Fully Tamperproof</li>
+        <li>Entirely Transparent</li>
+        <li>Non-Private</li>
+        <li>Censorship Resistant</li>
+        <br />
+        The design of the app is extremely simple. All you need is a Metamask
+        Account and boom you can create and donate to as many campaigns as you
+        want. Just connect your Metamask account with the `Connect` button on
+        the top right corner (toggle option for small devices) and raise funds
+        for your idea, the Web3 way...
       </p>
     </div>
   );
