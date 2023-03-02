c38afd55e942b56e31b8f5625d916d93d9f8b689
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-03-03 00:15:18 +0530

    Minor modification

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
         Welcome to DeFund
       </h1>
 
-      <p className="text-justify text-[#c8f7ec] font-epilogue text-[18px] mx-[3rem] my-4">
+      <p className="text-justify text-[#c5c9c8] font-epilogue text-[18px] mx-[3rem] my-4 ">
         This is where crowdfunding meets the world of Decentralized Web. Using
         DeFund, you can create customized campaigns representing your ideas and
         raise funds in crypto. DeFund is a Crowdfunding platform based on the
@@ -49,18 +49,24 @@ const Home = () => {
         under the hood implies that it comes with the perks of decentralization.{' '}
         <br />
         <br /> It's Salient Features are as follows:
-        <li>Completely Decentralized</li>
-        <li>Highly Secured</li>
-        <li>Fully Tamperproof</li>
-        <li>Entirely Transparent</li>
-        <li>Non-Private</li>
-        <li>Censorship Resistant</li>
+        <div className="text-[#23df84] text-transparent">
+          <li>Completely Decentralized</li>
+          <li>Highly Secured</li>
+          <li>Fully Tamperproof</li>
+          <li>Entirely Transparent</li>
+          <li>Non-Private</li>
+          <li>Censorship Resistant</li>
+        </div>
         <br />
         The design of the app is extremely simple. All you need is a Metamask
         Account and boom you can create and donate to as many campaigns as you
-        want. Just connect your Metamask account with the `Connect` button on
-        the top right corner (toggle option for small devices) and raise funds
-        for your idea, the Web3 way...
+        want. <br />
+        Just connect your Metamask account with the{' '}
+        <span className="bg-gradient-to-r from-[#8c6dfd] to-[#d47c34] font-epilogue font-semibold test-[16px] leading-[26px] text-white min-h-[52px] px-2 py-2 rounded-[10px]">
+          Connect
+        </span>{' '}
+        button on the top right corner (toggle option for small devices) and
+        raise funds for your idea, the Web3 way...
       </p>
     </div>
   );
